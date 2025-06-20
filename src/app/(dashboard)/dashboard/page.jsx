0f5bc01fca3f4fc75c4d445554e771149585e846
DarkAsfu <ashraful.islam.asfu@gmail.com>
2025-06-20 21:05:42 +0600

    Navbar design complete

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,9 +1,12 @@
+import Navbar from '@/app/modules/Dashboard/Navbar';
+import SectionBanner from '@/app/modules/Reusable/SectionBanner';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            Dashboard HOME
+            <Navbar/>
+            <SectionBanner/>
         </div>
     );
 };
